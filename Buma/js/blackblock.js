ee2431b0e862d5329ba82b122480030f9cca4242
jaker1 <jaker.raimov@mail.ru>
2019-08-17 20:12:56 +0500

    Buma

---
+++ Buma/js/blackblock.js
@@ -0,0 +1,19 @@
+//Header
+$("header nav ul li:nth-child(2)").on("click",function(){
+    $(this).find(".dropdown").slideToggle(200)
+});
+
+//Entry
+$(".entry .inner>img").toggleClass("");
+
+
+
+
+//if(window.innerWidth<700){
+//   $(".entry>img").attr("src","img/enter.png") 
+//}
+
+
+
+
+$( document ).ready()
